052e4e39717b2ca9f075736574e1d028e1492366
DspStudent <devisriprasad948@gmail.com>
2024-06-02 09:56:08 +0530

    test

--- src/test/java/backend/anime/AnimeApplicationTests.java
+++ src/test/java/backend/anime/AnimeApplicationTests.java
@@ -1,24 +1,11 @@
 package backend.anime;
 
-import backend.anime.email.EmailSevice;
-import backend.anime.entites.usersEntities.User;
-import backend.anime.entites.usersEntities.UserModal;
-import backend.anime.security.Auth.AuthService;
-import backend.anime.security.Auth.SignUpResponse;
-import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class AnimeApplicationTests {
-
-
 	@Test
 	void contextLoads() {
 	}
-
-
 }
