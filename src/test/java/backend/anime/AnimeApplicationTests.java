4e623fdbb2b35be1f3fac1a92637ccccb80e3063
DspStudent <devisriprasad948@gmail.com>
2024-06-02 09:34:04 +0530

    second commit

--- src/test/java/backend/anime/AnimeApplicationTests.java
+++ src/test/java/backend/anime/AnimeApplicationTests.java
@@ -20,39 +20,5 @@ class AnimeApplicationTests {
 	void contextLoads() {
 	}
 
-	@Autowired
-	EmailSevice emailSevice;
-	@MockBean
-	private AuthService authService;
 
-	@Test
-	public void signUpUserTest_success() {
-		UserModal user = UserModal.builder()
-				.firstName("Dev")
-				.secondName("Ranga")
-				.email("devisriprasad948@gmail.com")
-				.password("Devi")
-				.confirmPassword("Devi")
-				.build();
-
-		User expectedUser = User.builder() // Assuming User class exists
-				 // Set an ID if applicable
-				.firstName("Dev")
-				.secondName("Ranga")
-				.email("devisriprasad948@gmail.com")
-				.password("Devi")
-				.build();
-
-		Mockito.when(authService.signUpUser(user)).getMock();
-
-		authService.signUpUser(user);
-
-		// Verify that authService.signUpUser was called with the correct user
-		Mockito.verify(authService).signUpUser(user);
-	}
-
-	@Test
-	public void mailSendTo() throws MessagingException {
-		emailSevice.mailSendTo("devisriprasad948@gmail.com", "verfiy", "aa");
-	}
 }
