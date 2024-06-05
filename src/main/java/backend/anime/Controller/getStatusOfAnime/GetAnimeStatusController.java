c6f8b95a7c25e9e090cc784a3d504df74923a3a6
DspStudent <devisriprasad948@gmail.com>
2024-06-05 12:00:01 +0530

    login auth custom

--- src/main/java/backend/anime/Controller/getStatusOfAnime/GetAnimeStatusController.java
+++ src/main/java/backend/anime/Controller/getStatusOfAnime/GetAnimeStatusController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/lock/get")
 public class GetAnimeStatusController {
