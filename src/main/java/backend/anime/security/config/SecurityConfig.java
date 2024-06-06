7f81e34fe8c8c4aa4b4fbaf75d8863e360575e66
DspStudent <devisriprasad948@gmail.com>
2024-06-06 04:59:33 -0700

    login

--- src/main/java/backend/anime/security/config/SecurityConfig.java
+++ src/main/java/backend/anime/security/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public  CorsConfigurationSource configuration(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("https://anime-html-13pe.vercel.app","*")); // Replace with your exact frontend origin
+        config.setAllowedOrigins(Arrays.asList("*")); // Replace with your exact frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow common HTTP methods
         config.setAllowedHeaders(List.of("Content-Type", "Authorization", "X-Requested-With")); // Allow necessary headers
         config.setAllowCredentials(true); // Allow sending cookies if needed (be cautious)
